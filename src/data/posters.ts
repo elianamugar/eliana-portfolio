502d7b920940ecca6a8b0b094db4dbcbb0d069ab
elianamugar <emugar@bu.edu>
2026-05-19 21:45:51 -0700

    Added Posters section

---
+++ src/data/posters.ts
@@ -0,0 +1,57 @@
+export const posters = [
+  {
+    slug: "antidote",
+    image: "ANIDOTE+MOVIE+POSTER_EDIT_vertical+(1) copy.png",
+    title: "Antidote",
+    date: "2022-10",
+  },
+
+  {
+    slug: "abc",
+    image: "ABC_Poster_copy.webp",
+    title: "ABC",
+    date: "2024-06",
+  },
+
+  {
+    slug: "at what cost",
+    image: "AWC_Poster_004.webp",
+    title: "At What Cost",
+    date: "2023-11",
+  },
+
+  {
+    slug: "day of judgment",
+    image: "DayOfJudgement_MoviePoster11x14.webp",
+    title: "Day of Judgment",
+    date: "2023-03",
+  },
+
+  {
+    slug: "heartbeat by heartbeat",
+    image: "HbH_Poster_3.webp",
+    title: "Heartbeat by Heartbeat",
+    date: "2023-11",
+  },
+
+  {
+    slug: "teddys day off",
+    image: "TeddysDayOff_MoviePoster11x14_Black.png",
+    title: "Teddy's Day Off",
+    date: "2022-12",
+  },
+
+  {
+    slug: "dollhouse",
+    image: "Dollhouse_Poster_2 copy-01.png",
+    title: "Dollhouse",
+    date: "2022-12",
+  },
+
+  {
+    slug: "antidote alt",
+    image: "Antidote_Poster_02-01.png",
+    title: "Antidote Alternative Poster",
+    date: "2022-10",
+  },
+];
